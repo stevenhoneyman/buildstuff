4efb6681a5e798ab3d87600564a79fbaf1ce10cd
Steven Honeyman <stevenhoneyman@users.noreply.github.com>
2014-10-02 22:53:52 +0100

    included a lot of git repo urls (messy)

--- buildstuff.sh
+++ buildstuff.sh
@@ -68,25 +68,68 @@ function get_source() {
 	musl) 		url="git://git.musl-libc.org/musl" ;;
 	busybox)	url="git://git.busybox.net/busybox" ;;
 	*-headers)	url="git://github.com/sabotage-linux/kernel-headers.git" ;;
+
+#	acl)		url="git://git.sv.gnu.org/acl.git" ;;
+#	attr)		url="git://git.sv.gnu.org/attr.git" ;;
+#	bash)		url="git://git.sv.gnu.org/bash.git" ;;
+#	coreutils)	url="git://git.sv.gnu.org/coreutils.git" ;;
+#	cryptsetup)	url="git://git.kernel.org/pub/scm/utils/cryptsetup/cryptsetup.git" ;;
+#	cv)		url="git://github.com/Xfennec/cv.git" ;;
+#	dash)		url="git://git.kernel.org/pub/scm/utils/dash/dash.git" ;;
+#	diffutils)	url="git://git.sv.gnu.org/diffutils.git" ;;
 	dropbear)	url="git://github.com/mkj/dropbear.git" ;;
+#	e2fsprogs)	url="git://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git" ;;
+#	ethtool)	url="git://git.kernel.org/pub/scm/network/ethtool/ethtool.git" ;;
+#	file)		url="git://github.com/file/file.git" ;;
+#	findutils)	url="git://git.sv.gnu.org/findutils.git" ;;
+#	gawk)		url="git://git.sv.gnu.org/gawk.git" ;;
+#	gzip)		url="git://git.sv.gnu.org/gzip.git" ;;
+#	hexedit)	url="git://github.com/pixel/hexedit.git" ;;
 	htop)		url="git://github.com/hishamhm/htop.git" ;;
+#	icoutils)	url="git://git.sv.gnu.org/icoutils.git" ;;
+#	iproute2)	url="git://git.kernel.org/pub/scm/linux/kernel/git/shemminger/iproute2.git" ;;
+#	iptables)	url="git://git.netfilter.org/iptables.git" ;;
+#	kmod)		url="git://git.kernel.org/pub/scm/utils/kernel/kmod/kmod.git" ;;
+#	libnl-tiny)	url="git://github.com/sabotage-linux/libnl-tiny.git" ;;
+#	libpng)		url="git://git.code.sf.net/p/libpng/code" ;;
 	make)		url="git://git.sv.gnu.org/make.git" ;;
+#	mksh)		url="git://github.com/MirBSD/mksh.git" ;;
+#	multitail)	url="git://github.com/flok99/multitail.git" ;;
+#	nasm)		url="git://repo.or.cz/nasm.git" ;;
+#	openssl)	url="git://git.openssl.org/openssl.git" ;;
+#	patch)		url="git://git.sv.gnu.org/patch.git" ;;
 	pkgconf)	url="git://github.com/pkgconf/pkgconf.git" ;;
+#	readline)	url="git://git.sv.gnu.org/readline.git" ;;
+#	screen)		url="git://git.sv.gnu.org/screen.git" ;;
+#	sed)		url="git://git.sv.gnu.org/sed.git" ;;
+#	sstrip)		url="git://github.com/BR903/ELFkickers.git" ;;
+	strace)		url="git://git.code.sf.net/p/strace/code" ;;
+#	tar)		url="git://git.sv.gnu.org/tar.git" ;;
+#	tcc)		url="git://repo.or.cz/tinycc.git" ;;
+#	util-linux)	url="git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git" ;;
+#	wget)		url="git://git.sv.gnu.org/wget.git" ;;
+#	yasm)		url="git://github.com/yasm/yasm.git" ;;
 	zlib)		url="git://github.com/madler/zlib.git" ;;
 
-	## there's always a few awkward ones...
-	ncurses)	wget -nv ftp://invisible-island.net/ncurses/current/${_ncurses:-ncurses.tar.gz} -O - | tar zxf - -C "$_tmp" && mv "$_tmp"/${1}-* "$_tmp"/${1}-src ;;
+	# there's always a few awkward ones...
+#	bc) 		wget -nv ftp://alpha.gnu.org/gnu/bc/bc-1.06.95.tar.bz2 -O-|tar jxf - -C "$_tmp" && mv "$_tmp"/${1}-* "$_tmp"/${1}-src ;;
+#	cpuid) 		wget -nv http://etallen.com/${1}/$(wget -qO- "http://etallen.com/$1/?C=M;O=D;F=1;P=$1*src*"|grep -om1 "$1.*gz") -O-|tar zxf - -C "$_tmp" && mv "$_tmp"/${1}-* "$_tmp"/${1}-src ;;
+#	distcc) 	svn co -q http://distcc.googlecode.com/svn/trunk/ "$_tmp/distcc-src" ;;
+#	less)		echo "TODO" http://www.greenwoodsoftware.com/less/less-458.tar.gz ;;
 	nano) 		svn co -q svn://svn.savannah.gnu.org/nano/trunk/nano "$_tmp/nano-src" ;;
+	ncurses)	wget -nv ftp://invisible-island.net/ncurses/current/${_ncurses:-ncurses.tar.gz} -O - | tar zxf - -C "$_tmp" && mv "$_tmp"/${1}-* "$_tmp"/${1}-src ;;
+#	netcat) 	svn co -q svn://svn.code.sf.net/p/netcat/code/trunk "$_tmp/netcat-src" ;;
+#	pax-utils) 	(cd "$_tmp" && cvs -qd :pserver:anonymous@anoncvs.gentoo.org:/var/cvsroot co -d ${1}-src gentoo-projects/${1}) ;;
 	popt) 		(cd "$_tmp" && cvs -qd :pserver:anonymous@rpm5.org:/cvs co -d popt-src popt) ;;
 
-	*) echo "$1 is not a recognized source name" ;;
+	*) url="no" ;;
   esac
 
   [ $url != "no" ] && \
 	git clone --single-branch --depth=${_gitdepth} $url "${_tmp}/${1}-src" &
 }
 
-for src in musl musl-kernel-headers busybox make ncurses pkgconf popt zlib htop nano dropbear ; do
+for src in musl musl-kernel-headers busybox acl attr bash coreutils cryptsetup cv dash diffutils dropbear e2fsprogs ethtool file findutils gawk gzip hexedit htop icoutils iptables kmod libnl-tiny libpng make mksh multitail nasm ncurses openssl patch pkgconf readline screen sed sstrip strace tar tcc util-linux wget yasm bc cpuid distcc nano netcat pax-utils popt zlib; do
 	if [ -d "$_gitdir/$src" ]; then
 		msg3 "Updating $_gitdir/$src"; cd "$_gitdir/$src" && git pull
 		msg6 "Copying $src source"; cp -r "$_gitdir/$src" "$_tmp/${src}-src"
